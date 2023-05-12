4ab87b56663019ad6630a43bcbcc9fffb2d5d4d1
Joseph Makram <jojomak350@gmail.com>
2023-05-12 16:46:58 +0300

    Add validation

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"regexp"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
@@ -30,8 +31,10 @@ func main() {
 
 			if update.Message.Text == "/start" {
 				msg_content = fmt.Sprintf("Welcome to whatsapp helper you could call any number without saving it by writing the phone number ex: 012xxxxxxxx")
-			} else {
+			} else if valid, _ := regexp.MatchString("^01[0125][0-9]{8}$", update.Message.Text); valid == true {
 				msg_content = fmt.Sprintf("https://wa.me/+2%s", update.Message.Text)
+			} else {
+				msg_content = fmt.Sprintf("[%s] is not a valid egyptian phone number", update.Message.Text)
 			}
 
 			msg := tgbotapi.NewMessage(update.Message.Chat.ID, msg_content)
